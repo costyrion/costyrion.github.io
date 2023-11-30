65e73a89f632481a01fd4e981b4de4728c67147a
Piotr Wysocki <pan@piotrus.io>
2023-11-30 07:45:51 +0100

    ts component to mdx home page

---
+++ components/home/index.tsx
@@ -0,0 +1,10 @@
+import { Introduction } from './introduction';
+
+
+export function Home() {
+  return (
+    <main className="space-y-40 mb-40">
+      <Introduction />
+    </main>
+  );
+}
---
+++ components/home/introduction.tsx
@@ -0,0 +1,18 @@
+import { Container } from './container';
+
+
+export function Introduction() {
+  return (
+    <div className="relative">
+      <Container>
+        <div className="relative pt-20 md:pt-36 ml-auto">
+          <div className="lg:w-[70%] text-center mx-auto">
+            <h1 className="text-zinc-900 dark:text-white font-extrabold text-5xl md:text-6xl xl:text-7xl">
+              Project Costyrion
+            </h1>
+            </div>
+          </div>
+      </Container>
+    </div>
+  );
+}
